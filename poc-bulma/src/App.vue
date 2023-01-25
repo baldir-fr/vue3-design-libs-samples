270bea2422c6deae3b4561bd6f441707b1b9e957
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-01-25 20:03:32 +0100

    bulma

--- poc-bulma/src/App.vue
+++ poc-bulma/src/App.vue
@@ -3,8 +3,23 @@
 </script>
 
 <template>
-  <h1>Bulma</h1>
-  <p>TODO</p>
+
+  <h1 class="title">Bulma</h1>
+  <h2 class="subtitle">Subtitle</h2>
+  <p class="block">Lorem Elsass ipsum consectetur Strasbourg Huguette rhoncus morbi leo Hans mamsell aliquam id Pfourtz ! tellus
+    turpis, risus, gravida tellus vielmols, adipiscing Gal ! libero. geïz Mauris quam, Wurschtsalad condimentum schneck
+    lotto-owe hopla barapli amet Richard Schirmeck habitant sed id, bissame wie lacus hopla und auctor, commodo DNA, non
+    jetz gehts los hoplageiss schpeck gal purus quam. Morbi chambon mollis nüdle sit gewurztraminer tristique eget
+    Kabinetpapier sit rossbolla merci vielmols bredele picon bière nullam sagittis non réchime sit senectus Salu bissame
+    ac Chulien kuglopf schnaps ftomi! ullamcorper Chulia Roberstau Heineken et tchao bissame Christkindelsmärik Racing.
+    libero, wurscht ac Yo dû. knack dolor hopla ornare Carola yeuh. semper amet vulputate libero, in, météor varius
+    Oberschaeffolsheim kartoffelsalad turpis suspendisse elementum hopla knepfle porta s'guelt ch'ai munster
+    Pellentesque amet, Coopé de Truchtersheim dui hop eleifend ante dignissim pellentesque rucksack kougelhopf leo
+    Miss Dahlias blottkopf, elit so flammekueche mänele Verdammi placerat Gal. messti de Bischheim sed leverwurscht
+    Oberschaeffolsheim Salut bisamme baeckeoffe Spätzle salu geht's</p>
+  <button class="button is-primary">
+    Button
+  </button>
 </template>
 
 <style scoped>
